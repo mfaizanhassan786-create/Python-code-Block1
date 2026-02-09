40d6849d9b351573c3c4820b55e3b99d9d412c40
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-02-09 21:25:28 +0500

    Updated Variables file

---
+++ String_function.py
@@ -0,0 +1,55 @@
+
+#String end with:
+
+str1= "I am a student and I am learning the Python language."
+print(str1.endswith("uage."))
+
+
+#String start with:
+
+str2= "Python"
+print(str2.startswith("Py"))
+
+#String Capitalize:
+
+str1= "google is best for the learners"
+print(str1.capitalize())
+
+
+#String Replace:
+
+str1= "Gaagle is best far the schaal students."
+print(str1.replace("a","o"))
+
+
+#String Find:
+
+str1= "Python is a programming language."
+print(str1.find("programming"))
+
+
+#String Find:
+
+str1= "Python is a programming language."
+print(str1.find("is"))
+
+#String Count:
+
+str1= "Python is a programming language."
+print(str1.count("a"))
+
+#String Split:
+
+str1= "Python is a programming language."
+print(str1.split())
+
+#String Join:
+
+str1= "Python is a programming language."
+print(str1.join(" "))
+
+#String common
+
+str2= "Python"
+print(str2)
+
