4847706e398387fd5ec2056cc2d328acfcd109f6
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-02-11 10:46:44 +0500

    update

---
+++ 13_Conditions.py
@@ -0,0 +1,13 @@
+#In the every languages the conditional statements are used.
+#The coditional Statements have the types:
+#1. Squantial Statements
+#2. Conditional Statements or Selectional Statements
+#3. Iterative Statements
+#4. Rapetational Statements
+
+# And the conditional statements are used to make the decision in the program.
+
+# There are the two types of conditional statements:
+#1. if-else
+#2. if-elif-else
+#3. Nested if-else
